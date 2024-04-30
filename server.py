7443ed0e1a18af9f1ee0bc4bf541d6b2699101d5
Zezriel <60557470+Zezriel@users.noreply.github.com>
2024-04-30 19:00:30 +0200

    s

--- server.py
+++ server.py
@@ -3,25 +3,24 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def home():
+    if request.method == "POST":
+        
+        global player_name
+        player_name = request.form["player_name"]
+        
+        
+        return render_template("play.html")
     return render_template("home.html")
+
 
 
 @app.route("/play", methods=["GET","POST"])
 def play():
-    # La fonction play() peut à présent être employée avec une requpete GET (affichage classique dans le navigateur),
-    # mais également pour les requêtes POST (envoi d'informations).
-    player_name = ""
-    if request.method == "POST":
-        
-        # On récupère les valeurs saisies dans le champ correspondant au name "mon_champ"
-        player_name = request.form["player_name"]
-        name_output = "Bienvenu"+player_name
-        
-        # Traitement
-        # ...
 
-    return render_template("play.html",name_output = name_output)
+
+
+    return render_template("play.html")
 
 
