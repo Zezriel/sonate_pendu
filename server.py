66604aa2eb6f0c362ebe59db1ebf143230aaf6d7
Zezriel <60557470+Zezriel@users.noreply.github.com>
2024-04-30 22:40:47 +0200

    save

--- server.py
+++ server.py
@@ -5,19 +5,10 @@ from unidecode import unidecode
 app = Flask(__name__)
 
 
-dico = open("dictionnaire.txt", encoding="utf-8")
-liste = dico.readlines()
-dico.close()
-rdm = randint(0,len(liste))
-mot = liste[rdm].split(";")
-mot = mot[0]
-mot_decode = unidecode(mot)
-vie = 5
 
-liste_indice = []
-for x in range(len(mot)):
-    liste_indice.append("_")
-indice = "".join(liste_indice)
+
+
+
 
 
 
@@ -28,10 +19,12 @@ def home():
         
         global player_name
         player_name = request.form["player_name"]
+
         
         
         return render_template("play.html")
     return render_template("home.html")
+
 
 
 
@@ -42,16 +35,52 @@ def play():
 
 
 
-    return render_template("play.html",indice = indice)
+    if request.method == "GET":
+        dico = open("dictionnaire.txt", encoding="utf-8")
+        liste = dico.readlines()
+        dico.close()
+        rdm = randint(0,len(liste))
+        global mot
+        mot = liste[rdm].split(";")
+        mot = mot[0]
+        global mot_decode
+        mot_decode = unidecode(mot)
+        global vie
+        vie = 5
+        global liste_indice
+        liste_indice = []
+        for x in range(len(mot)):
+            liste_indice.append("_")
+        global indice
+        indice = "".join(liste_indice)
+        return render_template("play.html",indice = indice,mot = mot, vie = vie)
+        
+
+    if request.method == "POST":
+        player_input = request.form["letter"]
+        if player_input in mot_decode:
+            cherche = [pos for pos, char in enumerate(mot_decode) if char == player_input]
+            for x in cherche:
+
+                liste_indice[x] = mot[x]
+            indice = "".join(liste_indice)
+        else:
+
+            vie -= 1
+        if vie == 0:
+            return render_template("/loss", mot = mot)
+        
+        if "_" not in indice:
+            return render_template("/won", mot = mot)
+        return render_template("play.html",indice = indice,mot = mot, vie = vie)
 
 
 
-@app.route('/button_click', methods=["GET",'POST'])
-def button_click():
-    letter = request.form["letter"]
-    print(letter)
-    return render_template("play.html",indice = indice)
 
+@app.route("/loss", methods=["GET"])
+def loss():
+    return render_template("loss.html")
 
-
-
+@app.route("/won", methods=["GET"])
+def won():
+    return render_template("won.html")
